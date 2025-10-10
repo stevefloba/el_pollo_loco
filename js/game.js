79e973fbde976c069a9247b0ba3f155b1bea6772
Stefan Flosbach <stefanflosbach@t-online.de>
2025-10-10 20:52:37 +0200

    Bilder eingefügt. Character anzeigen.

--- js/game.js
+++ js/game.js
@@ -1,13 +1,13 @@
 let canvas;
-let ctx;
-
-let world = new World();
+let world;
 
 
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
+    
 
-console.log('My Character is', world.character);
+    console.log('My Character is', world.character);
+
 
 }
--- models/world.class.js
+++ models/world.class.js
@@ -6,7 +6,12 @@ class World {
         new Chicken()
     ];
 
-    draw() {
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+this.draw();
+    }
 
+    draw() {
+this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)
     }
 }
