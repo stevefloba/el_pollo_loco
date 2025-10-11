f99b6af3b0b523e1be6e5a68b35e060db7dbf2ca
Stefan Flosbach <stefanflosbach@t-online.de>
2025-10-11 19:18:59 +0200

    reguest Animation Frame. Chicken platzieren. Cloud einbinden. Hintergrundobjekte und Architektur optimiert.

--- models/world.class.js
+++ models/world.class.js
@@ -6,12 +6,45 @@ class World {
         new Chicken()
     ];
 
+    clouds = [
+        new Cloud()
+    ];
+
+    backgroundObjects = [
+        new BackgroundObject('../img/5_background/layers/1_first_layer/1.png', 0, 100)
+    ];
+
+    canvas;
+    ctx;
+
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
-this.draw();
+        this.canvas = canvas;
+        this.draw();
     }
 
     draw() {
-this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.backgroundObjects);
+
+        // Draw() wird immer wieder aufgerufen
+        let self = this; // in diesem speziellen Fall funktioniert this. nicht. Daher eine Variable damit zuordnen.
+        requestAnimationFrame(function () {
+            self.draw();
+        });
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
     }
 }
