f37a763bf26d9e138146b6ec57b316c159c7430a
Stefan Flosbach <stefanflosbach@t-online.de>
2025-10-12 21:44:25 +0200

    Mathematische Optimierungen. Hintergrundreihenfolge. Weitere Hintergrundobjekte. Himmel einfügen. Objekte skalieren.

--- models/world.class.js
+++ models/world.class.js
@@ -11,7 +11,10 @@ class World {
     ];
 
     backgroundObjects = [
-        new BackgroundObject('../img/5_background/layers/1_first_layer/1.png', 0, 100)
+        new BackgroundObject('../img/5_background/layers/air.png', 0),
+        new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('../img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('../img/5_background/layers/1_first_layer/1.png', 0)
     ];
 
     canvas;
@@ -26,10 +29,11 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
-        this.addObjectsToMap(this.backgroundObjects);
+
 
         // Draw() wird immer wieder aufgerufen
         let self = this; // in diesem speziellen Fall funktioniert this. nicht. Daher eine Variable damit zuordnen.
