2cf301d7886469937613d4bda4e9eecf2f08d9c5
Stefan Flosbach <stefanflosbach@t-online.de>
2025-10-13 16:59:06 +0200

    Image Cache. Character animieren. Chicken animieren.

--- models/movable-object-class.js
+++ models/movable-object-class.js
@@ -3,12 +3,22 @@ class MovableObject {
     y = 280;
     img;
     height = 150;
-    width = 100; 
+    width = 100;
+    imageCache = {};
+    currentImage = 0;
 
     // loadImage('img/test.png);
     loadImage(path) {
         this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
         this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
