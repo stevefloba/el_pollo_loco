398dfd45dbbf2b216299707b889d41d9d626b359
Stefan Flosbach <stefanflosbach@t-online.de>
2025-10-13 16:03:00 +0200

    Wolken bewegen

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,6 +7,13 @@ class Cloud extends MovableObject {
         super().loadImage('../img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.1;
+        }, 1000 / 60);
     }
 }
 
