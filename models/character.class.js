d9a0f29c7d5c96eb39fa0ea35e549e862b3606b9
Stefan Flosbach <stefanflosbach@t-online.de>
2025-10-14 19:55:12 +0200

    Endboss einfügen

--- models/character.class.js
+++ models/character.class.js
@@ -41,10 +41,7 @@ class Character extends MovableObject {
 
             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 // Walk animation
-                let i = this.currentImage % this.IMAGES_WALKING.length;
-                let path = this.IMAGES_WALKING[i];
-                this.img = this.imageCache[path];
-                this.currentImage++;
+this.playAnimation(this.IMAGES_WALKING);
             }
         }, 50);
     }
