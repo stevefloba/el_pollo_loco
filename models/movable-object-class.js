917c94957698c3470ef134de8de8192e060dee09
Stefan Flosbach <stefanflosbach@t-online.de>
2025-10-15 17:54:53 +0200

    Jump Animationen anzeigen. Pepe  soll springen.

--- models/movable-object-class.js
+++ models/movable-object-class.js
@@ -13,7 +13,7 @@ class MovableObject {
 
     applyGravity() {
         setInterval(() => {
-            if (this.isAboveGround()) {
+            if (this.isAboveGround() || this.speedY > 0) {
                 this.y -= this.speedY
                 this.speedY -= this.acceleration;
             }
@@ -46,12 +46,14 @@ class MovableObject {
     }
 
     moveRight() {
-        console.log('Moving right');
+        this.x += this.speed;
     }
 
     moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60);
+        this.x -= this.speed;
+    }
+
+    jump() {
+        this.speedY = 30;
     }
 }
